f41eac068ab1ba9279d0188573e5a3619b01a56a
mmmoikim <mmmoikim@gmail.com>
2019-04-02 17:49:47 +0900

    sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,35 @@
 module.exports = {
     title: 'MOILOG',
     description: 'mmmoilog',
-    base: '/moilog/'
+    base: '/moilog/',
+    themeConfig: {
+        repo: 'mmmoikim/moilog',
+        editLinks: true,
+        docsDir: 'docs',
+        lastUpdated: 'Last Updated',
+        search: false,
+        nav: [{
+            text: 'Home',
+            link: '/'
+        }],
+        sidebar: [{
+            title: 'VUE',
+            collapsable: true,
+            children: [
+                '/vue/'
+            ]
+        }, {
+            title: 'WEBPACK',
+            collapsable: true,
+            children: [
+                '/webpack/'
+            ]
+        }, {
+            title: 'GIT',
+            collapsable: true,
+            children: [
+                '/git/'
+            ]
+        }]
+    }
 }
